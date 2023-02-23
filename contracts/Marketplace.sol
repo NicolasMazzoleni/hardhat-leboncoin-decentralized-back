883ac22c8e70b94792fc10a5f1bd6013d38da51c
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2023-02-23 20:22:17 +0100

    ADD registerItem() function

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,6 +4,86 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Marketplace is ReentrancyGuard, Ownable  {
+error Marketplace__PriceMustBeGreaterThanZero();
+error Marketplace__ItemAlreadyListed();
 
+contract Marketplace is ReentrancyGuard, Ownable  {
+    uint private latestItemId;
+
+    // Creating a custom type about listing items in the marketplace
+    struct Listing {
+        uint256 price;
+        address seller;
+        bytes32 title;
+        bytes32 category;
+        bytes32 condition;
+        bytes32 description;
+        bytes32 localisation;
+    }
+
+    event ItemListed(
+        uint256 id,
+        address indexed caller,
+        bytes32 indexed title,
+        bytes32 indexed category,
+        bytes32 condition,
+        bytes32 description,
+        bytes32 localisation,
+        uint256 price
+    );
+
+    event ItemBought(
+        uint256 id,
+        address indexed caller,
+        bytes32 indexed title,
+        bytes32 indexed category,
+        bytes32 condition,
+        bytes32 description,
+        bytes32 localisation,
+        uint256 price
+    );
+
+    event ItemCancelled(
+        uint256 id,
+        address indexed caller,
+        bytes32 indexed title,
+        bytes32 indexed category,
+        bytes32 condition,
+        bytes32 description,
+        bytes32 localisation
+    );
+
+    // Global variables
+    // uint256 : Unique indentifier generated when listing new item
+    mapping(uint256 => Listing) private s_listing;
+
+    // Main functions
+    function registerItem(
+        bytes32 title,
+        bytes32 category,
+        bytes32 condition,
+        bytes32 description,
+        bytes32 localisation,
+        uint256 price) external onlyOwner {
+
+        address contractCallerAddress = msg.sender;
+
+        if (price <= 0) {
+            revert Marketplace__PriceMustBeGreaterThanZero();
+        }
+
+        uint256 itemId = getItemId();
+        Listing memory listing = s_listing[itemId];
+        if (listing.price > 0) {
+            revert Marketplace__ItemAlreadyListed();
+        }
+
+        s_listing[itemId] = Listing(price, contractCallerAddress, title, category, condition, description, localisation);
+        emit ItemListed(itemId, contractCallerAddress, title, category, condition, description, localisation, price);
+    }
+
+
+  function getItemId() internal returns (uint) {
+    return latestItemId++;
+  }
 }
