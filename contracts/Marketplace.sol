24f0f7e547185d2e0a617ba3480cddde0bccce95
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2023-02-23 19:49:20 +0100

    ADD @openzeppelin/contracts

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Marketplace is ReentrancyGuard, Ownable  {
+
+}
